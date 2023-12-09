f96e91373af32b983260e30cc7697b9e4eb17f38
Prajwal Prasad <prajwalpr11@gmail.com>
2023-12-10 04:51:21 +0530

    Assignment 6

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -4,7 +4,7 @@ function LandingPage() {
     <div className="container">
       <div className="container">
         <h2>CS 5610 : Web Development </h2>
-        <h4>Spandana Reddy Dara : 002625721</h4>
+        
         <nav className="nav nav-tabs mt-2">
           <Link to="/Labs" className="nav-link">
             Labs
